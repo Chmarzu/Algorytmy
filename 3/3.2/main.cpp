25d2ecb9a62cea4a007952a1688e3bf798544017
WesouyPies <97252780+WesouyPies@users.noreply.github.com>
2022-03-09 22:07:34 +0100

    Update main.cpp

--- 3/3.2/main.cpp
+++ 3/3.2/main.cpp
@@ -8,10 +8,19 @@ int main() {
     int matrixFirst[w][w], matrixSec[w][w];
     cout << "Wypisz liczby dla macierzy pierwszej od lewej do prawej" << endl;
 
-    for (i = 0; i < w; i++) {
-        for (j = 0; j < w; j++) {
+    for (i = 0; i < w; i++)
+    {
+        for (j = 0; j < w; j++) 
+        {
             cin >> a;
             matrixFirst[i][j] = a;
         }
     }
+    
+    cout << "Tranzpozycja macierzy pierwszej" << endl;
+    for (i = 0; i < n; i++) {
+        
+            for (j = 0; j < n; j++)
+            cout << matFirst[j][i] <<" ";
+            cout << endl;
 }
