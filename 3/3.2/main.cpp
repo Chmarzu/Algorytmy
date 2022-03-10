b016a7229d56a28672b012d3429dd99741baa0dd
Chmarzu <lukaszch137@gmail.com>
2022-03-10 09:47:07 +0100

    Multiplication Draft

--- 3/3.2/main.cpp
+++ 3/3.2/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     unsigned short mode;
-    int w, j, i, a, n, det, pom;
+    int w, i, j, k, a, det, *pointer, *pointer2, *pointer3, pom = 0;
 
     cout << "Wybierz operacje:" << endl << "(1) Wyznacznik" << endl << "(2) Mnozenie macierzy" << endl
          << "(3) Transpozycja" << endl;
@@ -37,12 +37,42 @@ int main() {
             cout << det;
             break;
         case 2:
+            cout << "Wypisz liczby dla macierzy drugiej od lewej do prawej" << endl;
+
+            for (i = 0; i < w; i++) {
+                for (j = 0; j < w; j++) {
+                    cin >> a;
+                    matrixSec[i][j] = a;
+                }
+            }
+
+            /*
+            pointer = & matrixFirst[0][0];
             for (i = 0; i < w * w; ++i) {
                 for (j = 0; j < w; ++j) {
+                    for (k = 0; k < w; ++k) {
+
+                    }
                     pom = (matrixFirst[i][j] * matrixSec[i][j]);
                 }
                 matrixMulti[i][j] = pom;
             }
+            */
+
+            pointer2 = &matrixSec[0][0];
+            pointer3 = &matrixMulti[0][0];
+            for (j = 0; j < w - 1; j++) {
+                pointer = &matrixFirst[0][0];
+                for (i = 0; i < w; ++i) {
+                    pom += *pointer * *pointer2;
+                    pointer++;
+                    pointer2 += w;
+                }
+                *pointer3 = pom;
+                pointer3++;
+                pointer2 -= w + 2;
+            }
+
             break;
         case 3:
             for (i = 0; i < w; i++) {
