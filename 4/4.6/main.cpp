2aadddbcab77bb6a0c5aa64f9b5b5d8d05c9b181
Chmarzu <lukaszch137@gmail.com>
2022-03-30 18:52:38 +0200

    Finished 4.6

--- 4/4.6/main.cpp
+++ 4/4.6/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+using namespace std;
+
+#define ROZ 11
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char pesel[ROZ];
+    int i, suma = 0, wynik;
+
+    cout << "Podaj numer PESEL:" << endl;       //Pobranie PESEL-u
+    for (i = 0; i < ROZ; ++i) {
+        cin >> pesel[i];
+        if (pesel[i] < 48 || pesel[i] > 57) {       //Weryfikacja wprowadzonych wartosci
+            cout << "Nieprawidlowy PESEL.";
+            exit(0);
+        }
+    }
+
+    for (i = 0; i < ROZ; ++i) {     //Test PESEL-u
+        if (!i || i == 4 || i == 8 || i == 10)
+            suma += (pesel[i]  - 48) * 1;
+        else if (i == 1 || i == 5 || i == 9)
+            suma += (pesel[i]  - 48) * 3;
+        else if (i == 2 || i == 6)
+            suma += (pesel[i]  - 48) * 7;
+        else if (i == 3 || i == 7)
+            suma += (pesel[i]  - 48) * 9;
+    }
+    wynik = suma % 10;
+
+    if (!wynik)
+        cout << "Prawidlowy PESEL.";
+    else
+        cout << "Nieprawidlowy PESEL.";
     return 0;
 }
