4db1dce1ba993ee81275793880b447923d2cc205
Chmarzu <lukaszch137@gmail.com>
2022-04-07 09:46:44 +0200

    Improved 5.2

---
+++ 5/5.2/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int i, j, size, mode;
+
+    cout << "Podaj ilosc znakow do posortowania:" << endl;
+    cin >> size;
+    char notsortc[size], sortc[size];
+    float notsortf[size], sortf[size];
+
+    cout << endl << "Wypierz opcje:" << endl;
+    cout << "(1) Sortowanie liczb naturalnych (dotatnie, calkowite) lub znakow" << endl;
+    cout << "(2) Sortowanie liczb ujemnych lub zmiennoprzecinkowych" << endl;
+    cin >> mode;
+    cout << endl;
+
+    switch (mode) {
+        case 1:
+            for (i = 0; i < size; i++) {
+                cout << "Podaj element numer " << i + 1 << ":" << endl;
+                cin >> notsortc[i];
+            }
+
+            sortc[0] = notsortc[0];
+            for (i = 1; i < size; i++) {
+                sortc[i] = notsortc[i];
+                for (j = 0; j < size; j++) {
+                    if (sortc[j] > sortc[i])
+                        swap(sortc[j], sortc[i]);
+                }
+            }
+
+            cout << endl << "Posortowana tablica:" << endl;
+            for (i = 0; i < size; i++)
+                cout << sortc[i] << ' ';
+            break;
+        case 2:
+            for (i = 0; i < size; i++) {
+                cout << "Podaj element numer " << i + 1 << ":" << endl;
+                cin >> notsortf[i];
+            }
+
+            sortf[0] = notsortf[0];
+            for (i = 1; i < size; i++) {
+                sortf[i] = notsortf[i];
+                for (j = 0; j < size; j++) {
+                    if (sortf[j] > sortf[i])
+                        swap(sortf[j], sortf[i]);
+                }
+            }
+
+            cout << endl << "Posortowana tablica:" << endl;
+            for (i = 0; i < size; i++)
+                cout << sortf[i] << ' ';
+
+            break;
+        default:
+            cout << "Bledna wartosc!";
+            break;
+    }
+
+    cout << endl;
+    fflush(stdin);
+    getchar();
+
+    return 0;
+}
