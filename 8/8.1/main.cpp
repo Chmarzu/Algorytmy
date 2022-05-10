2fdd31779664c452ee2fa0f8bb127be7873dd186
Chmarzu <lukaszch137@gmail.com>
2022-05-10 22:36:37 +0200

    8.1 Almost IT

--- 8/8.1/main.cpp
+++ 8/8.1/main.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
+#include <fstream>
 #include <windows.h>
 
 using namespace std;
 
-void Sort(int size);
+void Sort(int size, string &file_name);
 void ch_merge_sort(char *array, int start, int end);
 void ch_merge(char *array, int start, int middle, int end);
+void SaveTime(clock_t time);
 
 int main() {
-    int size = 0;
+    string file_name;
+    int temp, size = 0;
 
-    do {        //Pobranie rozmiaru tablicy sortowania
-        cout << "Podaj ilosc znakow do posortowania:" << endl;
-        cin >> size;
+    cout << "Podaj plik do posotowania: " << endl;
+    cin >> temp;
 
-        if (size < 2)
-            cout << endl << "Blad!" << endl;
-        cout << endl;
-    } while (size < 2);
+    switch (temp) {
+        case 10:
+            file_name = "10.txt";
+            size = temp;
+            break;
+        /*case 10:
+            file_name = "test.txt";
+            size = temp;
+            break;
+        case 10:
+            file_name = "test.txt";
+            size = temp;
+            break;
+        case 10:
+            file_name = "test.txt";
+            size = temp;
+            break;*/
+    }
 
-    Sort(size);
+
+    Sort(size, file_name);
 
     cout << endl;
     fflush(stdin);
@@ -27,17 +44,22 @@ int main() {
     return 0;
 }
 
-void Sort(int size) {
-    int i, j, min;
+void Sort(int size, string &file_name) {
+    int i = 0, j, min;
     unsigned short mode;
     clock_t diff[3] = {0, 0, 0};
     char tab[size];
     char tab2[size], *ptab, pivot, left[size], *pleft, right[size], *pright;
+    fstream file;
 
-    for (i = 0; i < size; i++) {        //Pobranie elementow do sortowania
-        cout << "Podaj element numer " << i + 1 << ":" << endl;
-        cin >> tab[i];
+    file.open(file_name);
+    string line;
+    while (file.good()) {
+        getline(file, line);
+        tab[i] = line[0];
+        i++;
     }
+    file.close();
 
     do {
         cout << "Wybierz algorytm sortujacy:" << endl;
@@ -68,131 +90,134 @@ void Sort(int size) {
         cout << endl;
     } while (mode < 1 || mode > 5);
 
-    switch (mode) {
-        case 1:
-            diff[0] = clock();
+    for (int k = 0; k < 5; ++k) {
+        switch (mode) {
+            case 1:
+                diff[0] = clock();
 
-            for (i = 0; i < size; i++) {
-                for (j = 1; j < size; j++) {
-                    if (tab[j - 1] > tab[j])
-                        swap(tab[j - 1], tab[j]);
-                }
-            }
-
-            diff[1] = clock();
-            diff[2] = diff[1] - diff[0];
-            break;
-        case 2:
-            diff[0] = clock();
-
-            tab2[0] = tab[0];
-            for (i = 1; i < size; i++) {
-                tab2[i] = tab[i];
-                for (j = 0; j < size; j++) {
-                    if (tab2[j] > tab2[i])
-                        swap(tab2[j], tab2[i]);
-                }
-            }
-
-            for (i = 0; i < size; i++)
-                tab[i] = tab2[i];
-
-            diff[1] = clock();
-            diff[2] = diff[1] - diff[0];
-            break;
-        case 3:
-            diff[0] = clock();
-
-            for (i = 0; i < size - 1; i++) {        //Sortowanie
-                min = i;
-                for (j = i; j < size; j++) {
-                    if (tab[j] < tab[min]) {
-                        min = j;
+                for (i = 0; i < size; i++) {
+                    for (j = 1; j < size; j++) {
+                        if (tab[j - 1] > tab[j])
+                            swap(tab[j - 1], tab[j]);
                     }
                 }
-                swap(tab[min], tab[i]);
-            }
 
-            diff[1] = clock();
-            diff[2] = diff[1] - diff[0];
-            break;
-        case 4:
-            diff[0] = clock();
+                diff[1] = clock();
+                diff[2] = diff[1] - diff[0];
+                break;
+            case 2:
+                diff[0] = clock();
 
-            ptab = &tab[0];
-            pleft = &left[0];
-            pright = &right[0];
+                tab2[0] = tab[0];
+                for (i = 1; i < size; i++) {
+                    tab2[i] = tab[i];
+                    for (j = 0; j < size; j++) {
+                        if (tab2[j] > tab2[i])
+                            swap(tab2[j], tab2[i]);
+                    }
+                }
 
-            for (i = 0; i < size; i++) {
-                tab[size] = '\0';
-                left[i] = right[i] = '\0';
-            }
+                for (i = 0; i < size; i++)
+                    tab[i] = tab2[i];
 
-            pivot = tab[0];     //Wybor pivota
+                diff[1] = clock();
+                diff[2] = diff[1] - diff[0];
+                break;
+            case 3:
+                diff[0] = clock();
 
-            for (i = 1; i < size; i++) {        //Podzial na mniejsze tablice
-                if (tab[i] < pivot) {
-                    *pleft = tab[i];
+                for (i = 0; i < size - 1; i++) {        //Sortowanie
+                    min = i;
+                    for (j = i; j < size; j++) {
+                        if (tab[j] < tab[min]) {
+                            min = j;
+                        }
+                    }
+                    swap(tab[min], tab[i]);
+                }
+
+                diff[1] = clock();
+                diff[2] = diff[1] - diff[0];
+                break;
+            case 4:
+                diff[0] = clock();
+
+                ptab = &tab[0];
+                pleft = &left[0];
+                pright = &right[0];
+
+                for (i = 0; i < size; i++) {
+                    tab[size] = '\0';
+                    left[i] = right[i] = '\0';
+                }
+
+                pivot = tab[0];     //Wybor pivota
+
+                for (i = 1; i < size; i++) {        //Podzial na mniejsze tablice
+                    if (tab[i] < pivot) {
+                        *pleft = tab[i];
+                        pleft++;
+                    } else {
+                        *pright = tab[i];
+                        pright++;
+                    }
+                }
+
+                //Sortowanie mniejszych tablic
+                if (left[0] != '\0') {
+                    for (i = 0; i < size; i++) {
+                        for (j = 1; j < size; j++) {
+                            if (left[j - 1] > left[j] && left[j] != '\0')
+                                swap(left[j - 1], left[j]);
+                        }
+                    }
+                }
+
+                if (right[0] != '\0') {
+                    for (i = 0; i < size; i++) {
+                        for (j = 1; j < size; j++) {
+                            if (right[j - 1] > right[j] && right[j] != '\0')
+                                swap(right[j - 1], right[j]);
+                        }
+                    }
+                }
+
+                //Przeniesienie posortowanych elementow do glownej tablicy
+                pleft = &left[0];
+                while (*pleft != '\0') {
+                    *ptab = *pleft;
+                    ptab++;
                     pleft++;
-                } else {
-                    *pright = tab[i];
+                }
+
+                *ptab = pivot;
+                ptab++;
+
+                pright = &right[0];
+                while (*pright != '\0') {
+                    *ptab = *pright;
+                    ptab++;
                     pright++;
                 }
-            }
 
-            //Sortowanie mniejszych tablic
-            if (left[0] != '\0') {
-                for (i = 0; i < size; i++) {
-                    for (j = 1; j < size; j++) {
-                        if (left[j - 1] > left[j] && left[j] != '\0')
-                            swap(left[j - 1], left[j]);
-                    }
-                }
-            }
+                diff[1] = clock();
+                diff[2] = diff[1] - diff[0];
+                break;
+            case 5:
+                diff[0] = clock();
+                ch_merge_sort(&tab[0], 0, size - 1);
+                diff[1] = clock();
+                diff[2] = diff[1] - diff[0];
+                break;
+        }
 
-            if (right[0] != '\0') {
-                for (i = 0; i < size; i++) {
-                    for (j = 1; j < size; j++) {
-                        if (right[j - 1] > right[j] && right[j] != '\0')
-                            swap(right[j - 1], right[j]);
-                    }
-                }
-            }
+        cout << endl << "Posortowana tablica:" << endl;     //Wynik
+        for (i = 0; i < size; i++)
+            cout << tab[i] << ' ';
 
-            //Przeniesienie posortowanych elementow do glownej tablicy
-            pleft = &left[0];
-            while (*pleft != '\0') {
-                *ptab = *pleft;
-                ptab++;
-                pleft++;
-            }
-
-            *ptab = pivot;
-            ptab++;
-
-            pright = &right[0];
-            while (*pright != '\0') {
-                *ptab = *pright;
-                ptab++;
-                pright++;
-            }
-
-            diff[1] = clock();
-            diff[2] = diff[1] - diff[0];
-            break;
-        case 5:
-            diff[0] = clock();
-            ch_merge_sort(&tab[0], 0, size - 1);
-            diff[1] = clock();
-            diff[2] = diff[1] - diff[0];
-            break;
+        cout << endl << "Czas sortowania: " << diff[2];
+        SaveTime(diff[2]);
     }
-
-    cout << endl << "Posortowana tablica:" << endl;     //Wynik
-    for (i = 0; i < size; i++)
-        cout << tab[i] << ' ';
-
-    cout << endl << "Czas sortowania: " << diff[2];
 }
 
 void ch_merge_sort(char *array, int start, int end) {
@@ -244,4 +269,17 @@ void ch_merge(char *array, int start, int middle, int end) {
         cout << *(array + i) << " ";
     Sleep(2000);
     cout << endl;
+}
+
+void SaveTime(clock_t time) {
+    int temp = time;
+    char text[20];
+    text[0] = temp + 48;
+
+    ofstream file;
+    file.open("result.txt");
+    //for (int i = 0; i < 5; i++) {
+        file << text[0] << "\n";
+    //}
+    file.close();
 }
