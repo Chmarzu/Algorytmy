15b7f5ff1336dfb6db64eca180f5e186d905215d
Chmarzu <lukaszch137@gmail.com>
2022-05-29 14:43:06 +0200

    Changed array type into int

--- 9/9.1/main.cpp
+++ 9/9.1/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void Select(int size);
-void Find(char tab[], char left[], char right[], char *pleft, char *pright, int i, int j, int k, int *r, int size, int *amount);
+void Find(int tab[], int left[], int right[], int *pleft, int *pright, int i, int j, int k, int *r, int size, int *amount);
 
 int main() {
     int size = 0;
@@ -28,7 +28,7 @@ int main() {
 
 void Select(int size) {
     int i, j, k, r, amount = 0;
-    char tab[size], left[size], *pleft, right[size], *pright;
+    int tab[size], left[size], *pleft, right[size], *pright;
     pleft = &left[0];
     pright = &right[0];
 
@@ -41,45 +41,40 @@ void Select(int size) {
         left[i] = right[i] = '\0';
     }
 
-    if (size == 1) {
+    if (size == 1)
         cout << endl << tab[0];
-
-        cout << endl;
-        fflush(stdin);
-        getchar();
-
-        exit(0);
-    } else {
+    else {
         cout << "Podaj indeks poszukiwanego elementu:" << endl;
         cin >> k;
-    }
-    for (i = 0; i < size; i++) {
-        for (j = i + 1; j < size; j++) {
-            if (tab[i] > tab[j]) {     //Wybor pivota
-                r = i;
-                i = j = size;
+
+        for (i = 0; i < size; i++) {
+            for (j = i + 1; j < size; j++) {
+                if (tab[i] > tab[j]) {     //Wybor pivota
+                    r = i;
+                    i = j = size;
+                }
             }
         }
-    }
 
-    for (i = 0; i < size; i++) {        //Podzial na mniejsze tablice
-        if (tab[i] < tab[r]) {
-            *pleft = tab[i];
-            pleft++;
-            amount++;
-        } else {
-            *pright = tab[i];
-            pright++;
+        for (i = 0; i < size; i++) {        //Podzial na mniejsze tablice
+            if (tab[i] < tab[r]) {
+                *pleft = tab[i];
+                pleft++;
+                amount++;
+            } else {
+                *pright = tab[i];
+                pright++;
+            }
         }
-    }
 
-    if (amount < k)
-        Find(&left[0], &left[0], &right[0], pleft, pright, i, j, k, &r, size, &amount);
-    else
-        Find(&right[0], &left[0], &right[0], pleft, pright, i, j, k, &r, size, &amount);
+        if (amount < k)
+            Find(&left[0], &left[0], &right[0], pleft, pright, i, j, k, &r, size, &amount);
+        else
+            Find(&right[0], &left[0], &right[0], pleft, pright, i, j, k, &r, size, &amount);
+    }
 }
 
-void Find(char tab[], char left[], char right[], char *pleft, char *pright, int i, int j, int k, int *r, int size, int *amount) {
+void Find(int tab[], int left[], int right[], int *pleft, int *pright, int i, int j, int k, int *r, int size, int *amount) {
     for (i = 0; i < size; i++) {
         for (j = i + 1; j < size; j++) {
             if (tab[i] > tab[j]) {     //Wybor pivota
