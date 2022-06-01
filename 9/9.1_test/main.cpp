51a8fddfb760d49c2872fdb01c9f86adda5b5789
Chmarzu <lukaszch137@gmail.com>
2022-06-01 14:48:04 +0200

    Finished 9.1_test

--- 9/9.1_test/main.cpp
+++ 9/9.1_test/main.cpp
@@ -10,18 +10,19 @@ int main() {
     int k = 1;      //poszukiwany element
     int i = 0, j = 0, w;       //pomocnicze zmienne
     int arr[n];   //zbior elementow
-    fstream file;
-    string line;
-    char character[3] = {'\0', '\0', '\0'};
-    clock_t diff[3] = {0, 0, 0};
+    fstream file;   //wskaznik obslugujacy plik
+    string line;    //pomocnicza zmienna
+    clock_t diff[3] = {0, 0, 0};    //wartosci czasu: poczatek, koniec, roznica
 
     file.open("src1.txt");
     while (file.good()) {
         getline(file, line);
-        character[0] = line[0];
 
-        while (j < 3 && character[j] != '\0') {
-            arr[i] = character[j];
+        while (j < 3 && line[j] != '\0') {
+            if (!j)
+                arr[i] = line[j] - 48;
+            else
+                arr[i] = arr[i] * 10 + line[j] - 48;
             j++;
         }
 
@@ -38,7 +39,7 @@ int main() {
         n = k;
 
         while (i != j) {
-            w = partition(arr, i, j);
+            w = partition(arr, i, j);   //Podzial na podzbiory
             w = w - i + 1;
             if (w >= k) j = i + w - 1;
             if (w < k) {
